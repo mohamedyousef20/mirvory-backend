1f87ead7f0f27d76effc31818c6e6a082e43de75
mohamedyousef20 <mohamedyousefle@gmail.com>
2026-08-02 12:43:25 +0300

    Add logging for email configuration
    Log email configuration details including host, port, user, and password existence.

--- middlewares/email.middleware.js
+++ middlewares/email.middleware.js
@@ -21,6 +21,12 @@ const getTransporter = () => {
       socketTimeout: 5000,
     });
   }
+  console.log({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  user: process.env.EMAIL_USER,
+  passwordExists: !!process.env.EMAIL_PASSWORD,
+});
   return transporter;
 };
 
