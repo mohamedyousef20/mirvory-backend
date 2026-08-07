191216c23a6c09c530dfded4546541b89a215aa3
mohamedyousef20 <mohamedyousefle@gmail.com>
2026-08-07 17:58:24 +0300

    Update product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -48,7 +48,7 @@ router.delete('/', protect, isSeller, deleteProduct);
 router.patch('/', protect, isSeller, updateProduct);
 
 // مسارات عامة
-/ Get all products in category
+// Get all products in category
 router.get('/category/:categoryId', paginate(12), sort(), buildFilter(commonFilters.product), getProductsByCategory);
 // Advanced search endpoint
 router.get('/search', searchProducts);
